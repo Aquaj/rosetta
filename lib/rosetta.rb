59417357a3e7e67802e43a3240300b562cb2ccfb
Jérémie Bonal <jeremie.bonal@gmail.com>
2019-08-02 22:04:00 +0200

    First naive implementation.

--- lib/rosetta.rb
+++ lib/rosetta.rb
@@ -1,7 +1,20 @@
+require 'json'
+require 'csv'
+require 'byebug'
+
 class Rosetta
   class << self
     def convert(json)
-
+      j = JSON(json)
+      headers = -> (hash) { hash.flat_map { |key, val| val.is_a?(Hash) ? headers.(val).map{|head| [key, head].join(?.) } : key } }
+      content = -> (hash, key) { key.split(".").reduce(hash) { |c, k| c[k] } }
+      head = headers.(j.first)
+      CSV.generate do |csv|
+        csv << head
+        j.each do |obj|
+          csv << head.map { |h| c = content.(obj, h); c.is_a?(Array) ? c.join(?,) : c }
+        end
+      end
     end
   end
 end

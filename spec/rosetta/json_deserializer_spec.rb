e014a867a7a49a569b9fa85183444b21b1cfe3b9
Jérémie Bonal <jeremie.bonal@gmail.com>
2019-08-02 23:58:46 +0200

    Moved JSON specific specs to JSONSerializer spec.

--- spec/rosetta/json_deserializer_spec.rb
+++ spec/rosetta/json_deserializer_spec.rb
@@ -11,4 +11,29 @@ RSpec.describe Rosetta::JSONDeserializer do
     expect(deserialized.length).to be 2
     expect(deserialized).to satisfy { |result| result.all? { |e| e.is_a? Rosetta::Element } }
   end
+
+  it 'raises if passed invalid JSON as an input' do
+    input = File.read(file_fixture('users.json'))
+    input = input[0..-10] # Incomplete file
+    expect { Rosetta::JSONDeserializer.deserialize(input) }.to(
+      raise_error(Rosetta::DeserializationError))
+  end
+
+  it 'handles JSON arrays of objects only' do
+    non_objects = <<-JSON
+      [
+        ["a", 1]
+      ]
+    JSON
+    expect { Rosetta::JSONDeserializer.deserialize(non_objects) }.to(
+      raise_error(Rosetta::DeserializationError))
+
+    non_array = <<-JSON
+    {
+      "object": { "a": 1 }
+    }
+    JSON
+    expect { Rosetta::JSONDeserializer.deserialize(non_array) }.to(
+      raise_error(Rosetta::DeserializationError))
+  end
 end
--- spec/rosetta_spec.rb
+++ spec/rosetta_spec.rb
@@ -7,12 +7,6 @@ RSpec.describe Rosetta, '#convert' do
     expect(Rosetta.convert(input)).to eq(output)
   end
 
-  it 'raises if passed invalid JSON as an input' do
-    input = File.read(file_fixture('users.json'))
-    input = input[0..-10] # Incomplete file
-    expect { Rosetta.convert(input) }.to raise_error(Rosetta::ConversionError)
-  end
-
   it "raises if all objects in the JSON array don't share the same structure" do
     input = <<-JSON
       [
@@ -21,21 +15,5 @@ RSpec.describe Rosetta, '#convert' do
       ]
     JSON
     expect { Rosetta.convert(input) }.to raise_error(Rosetta::ConversionError)
-  end
-
-  it 'handles JSON arrays of objects only' do
-    non_objects = <<-JSON
-      [
-        ["a", 1]
-      ]
-    JSON
-    expect { Rosetta.convert(non_objects) }.to raise_error(Rosetta::ConversionError)
-
-    non_array = <<-JSON
-    {
-      "object": { "a": 1 }
-    }
-    JSON
-    expect { Rosetta.convert(non_array) }.to raise_error(Rosetta::ConversionError)
   end
 end

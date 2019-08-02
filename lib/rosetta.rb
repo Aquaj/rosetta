be19cc47dbbc45475b666f8e6197f22bce4a2103
Jérémie Bonal <jeremie.bonal@gmail.com>
2019-08-02 22:32:39 +0200

    Extracted argument validation to own method.

--- lib/rosetta.rb
+++ lib/rosetta.rb
@@ -6,18 +6,16 @@ class Rosetta
 
   class << self
     def convert(json)
-      raise ConversionError, "JSON input is invalid" unless j = valid_json(json)
-      raise ConversionError, "JSON input must be an array" unless j.is_a? Array
-      raise ConversionError, "JSON input must contain objects" unless j.all? { |o| o.is_a? Hash }
+      input = valid_input_from!(json)
       headers = -> (hash) { hash.flat_map { |key, val| val.is_a?(Hash) ? headers.(val).map{|head| [key, head].join(?.) } : key } }
       content = -> (hash, key) { key.split(".").reduce(hash) { |c, k| c[k] } }
-      head, *others = j.map(&headers).uniq
+      head, *others = input.map(&headers).uniq
       if others.any?
         raise ConversionError, "All objects in JSON array do not share the same structure"
       end
       CSV.generate do |csv|
         csv << head
-        j.each do |obj|
+        input.each do |obj|
           csv << head.map { |h| c = content.(obj, h); c.is_a?(Array) ? c.join(?,) : c }
         end
       end
@@ -25,6 +23,20 @@ class Rosetta
 
     private
 
+      def valid_input_from!(json)
+        raise ConversionError, <<-ERROR.chomp unless input = valid_json(json)
+          JSON input is invalid
+        ERROR
+        raise ConversionError, <<-ERROR.chomp unless input.is_a? Array
+          JSON input must be an array
+        ERROR
+        raise ConversionError, <<-ERROR.chomp unless input.all? { |o| o.is_a? Hash }
+          JSON input must contain objects
+        ERROR
+        input
+      end
+
+      #HACK: Feels dirty but there's no JSON soft-parsing in ruby's json lib
       def valid_json(json)
         JSON(json)
       #NOTE: Rescuing TypeError too in case json is not a String

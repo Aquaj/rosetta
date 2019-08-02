5e370ba1fe1b7165c0440ffd04f4b839943c539f
Jérémie Bonal <jeremie.bonal@gmail.com>
2019-08-02 22:13:38 +0200

    Failfast for bad JSON.

--- lib/rosetta.rb
+++ lib/rosetta.rb
@@ -5,6 +5,7 @@ require 'byebug'
 class Rosetta
   class << self
     def convert(json)
+      raise ArgumentError, "JSON input is invalid" unless j = valid_json(json)
       j = JSON(json)
       headers = -> (hash) { hash.flat_map { |key, val| val.is_a?(Hash) ? headers.(val).map{|head| [key, head].join(?.) } : key } }
       content = -> (hash, key) { key.split(".").reduce(hash) { |c, k| c[k] } }
@@ -16,5 +17,14 @@ class Rosetta
         end
       end
     end
+
+    private
+
+      def valid_json(json)
+        JSON(json)
+      #NOTE: Rescuing TypeError too in case json is not a String
+      rescue JSON::ParserError, TypeError
+        nil
+      end
   end
 end

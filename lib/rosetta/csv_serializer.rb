83b7b2409fef00af254340f6dfc141c1d6b09c07
Jérémie Bonal <jeremie.bonal@gmail.com>
2019-08-02 23:20:26 +0200

    Made it so serializer classes delegate to instances instead of having the logic at class level.

--- lib/rosetta/csv_serializer.rb
+++ lib/rosetta/csv_serializer.rb
@@ -1,18 +1,25 @@
 class Rosetta
   class CSVSerializer
-    class << self
-      def serialize(elements, headers)
-        CSV.generate do |csv|
-          csv << headers
-          elements.each do |element|
-            csv << headers.map do |header|
-              value = element[header]
-              case value
-              when Array
-                value.join(',')
-              else
-                value
-              end
+    def self.serialize(elements, headers)
+      new(elements, headers).serialize
+    end
+
+    def initialize(elements, headers)
+      @elements = elements
+      @headers = headers
+    end
+
+    def serialize
+      CSV.generate do |csv|
+        csv << @headers
+        @elements.each do |element|
+          csv << @headers.map do |header|
+            value = element[header]
+            case value
+            when Array
+              value.join(',')
+            else
+              value
             end
           end
         end

101d5178f29fb36d926c4ffa11bfda4bedeb06eb
Jérémie Bonal <jeremie.bonal@gmail.com>
2019-08-02 22:18:49 +0200

    Added check on schema unicity across JSON objects.

--- lib/rosetta.rb
+++ lib/rosetta.rb
@@ -3,13 +3,18 @@ require 'csv'
 require 'byebug'
 
 class Rosetta
+  class ConversionError < StandardError; end
+
   class << self
     def convert(json)
       raise ArgumentError, "JSON input is invalid" unless j = valid_json(json)
       j = JSON(json)
       headers = -> (hash) { hash.flat_map { |key, val| val.is_a?(Hash) ? headers.(val).map{|head| [key, head].join(?.) } : key } }
       content = -> (hash, key) { key.split(".").reduce(hash) { |c, k| c[k] } }
-      head = headers.(j.first)
+      head, *others = j.map(&headers).uniq
+      if others.any?
+        raise ConversionError, "All objects in JSON array do not share the same structure"
+      end
       CSV.generate do |csv|
         csv << head
         j.each do |obj|

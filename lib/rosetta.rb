64ac06e82507dd4295ed21936e01d8829744631f
Jérémie Bonal <jeremie.bonal@gmail.com>
2019-08-03 01:32:59 +0200

    Rename #convert to #translate for more coherence.

--- lib/rosetta.rb
+++ lib/rosetta.rb
@@ -2,7 +2,7 @@ require 'rosetta/translation'
 
 class Rosetta
   class << self
-    def convert(input, from: :csv, to: :json)
+    def translate(input, from: :csv, to: :json)
       Translation.new(from, to).call(input)
     end
   end
--- spec/rosetta_spec.rb
+++ spec/rosetta_spec.rb
@@ -1,9 +1,9 @@
 require 'rosetta'
 
-RSpec.describe Rosetta, '#convert' do
+RSpec.describe Rosetta, '#translate' do
   it 'takes a JSON array of structured objects and outputs it as a CSV table' do
     input = File.read(file_fixture('users.json'))
     output = File.read(file_fixture('users.csv'))
-    expect(Rosetta.convert(input)).to eq(output)
+    expect(Rosetta.translate(input)).to eq(output)
   end
 end

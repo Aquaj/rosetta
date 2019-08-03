99c629284f813724308aa6b263f8b7d145cb795d
Jérémie Bonal <jeremie.bonal@gmail.com>
2019-08-03 02:12:24 +0200

    Adjusted spec title to reflect the increase in genericity of Rosetta.

--- spec/rosetta_spec.rb
+++ spec/rosetta_spec.rb
@@ -1,7 +1,7 @@
 require 'rosetta'
 
 RSpec.describe Rosetta, '#translate' do
-  it 'takes a JSON array of structured objects and outputs it as a CSV table' do
+  it 'can translate from one format to another' do
     input = File.read(file_fixture('users.json'))
     output = File.read(file_fixture('users.csv'))
     expect(Rosetta.translate(input)).to eq(output)

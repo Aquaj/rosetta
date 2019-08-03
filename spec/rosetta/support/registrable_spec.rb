8e1609e9142f866d87d70100f575077c1c0d0941
Jérémie Bonal <jeremie.bonal@gmail.com>
2019-08-03 18:45:15 +0200

    Added spec for new Registerable module.

---
+++ spec/rosetta/support/registrable_spec.rb
@@ -0,0 +1,51 @@
+require 'rosetta/support/registerable'
+
+RSpec.describe Rosetta::Support::Registerable do
+  before(:each) do
+    class TestClass
+      extend Rosetta::Support::Registerable
+    end
+  end
+
+  it 'allows classes to have objects registered to them' do
+    TestClass.registerable_as :test
+
+    to_be_registered = Object.new
+    TestClass.register :toto, to_be_registered
+
+    expect(TestClass.registered[:toto]).to eq(to_be_registered)
+  end
+
+  it 'raises on registration key collision' do
+    TestClass.registerable_as :test
+
+    to_be_registered = Object.new
+    TestClass.register :toto, to_be_registered
+    expect { TestClass.register :toto, to_be_registered }.to(
+      raise_error(Rosetta::RegistrationError))
+  end
+
+  it 'allows for custom error classes' do
+    TestClass.registerable_as :test
+
+    class TestClass::ExistingTestError < StandardError; end
+
+    to_be_registered = Object.new
+    TestClass.register :toto, to_be_registered
+    expect { TestClass.register :toto, to_be_registered }.to(
+      raise_error(TestClass::ExistingTestError))
+  end
+
+  it 'provides a [] shorthand access method' do
+    TestClass.registerable_as :test
+
+    to_be_registered = Object.new
+    TestClass.register :toto, to_be_registered
+
+    expect(TestClass[:toto]).to eq(to_be_registered)
+  end
+
+  after(:each) do
+    Object.send(:remove_const, :TestClass)
+  end
+end
